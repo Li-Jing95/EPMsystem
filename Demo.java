4d9e13050f7a61dfc40e9d472866ff1f7c277ce3
Li-Jing95 <1443037346@qq.com>
2019-03-11 18:40:41 +0800

    登录窗口（权限没有实现）

---
+++ Demo.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Demo {
+    //构造函数
+    public Demo(){
+    }
+    public void createJFrame(){
+        JFrame jframe = new JFrame("登录");
+        jframe.setSize(500, 450);
+        jframe.setVisible(true);
+    }
+}
